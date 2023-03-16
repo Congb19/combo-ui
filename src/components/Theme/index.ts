10b5c2d6e571f44de2f5a47497e22616f59d51d4
congb19 <347006087@qq.com>
2023-03-16 14:33:29 +0800

    feat: 完善theme system

--- src/components/Theme/index.ts
+++ src/components/Theme/index.ts
@@ -1,50 +1,72 @@
 import { ref, watch } from 'vue-demi'
-import colors from '../../styles/colors.css'
-import colorsDark from '../../styles/colors-dark.css'
+import '../../styles/colors.css'
+import '../../styles/colors-dark.css'
 
-// theme system
-const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-const currentTheme = ref(isDark ? 'dark' : 'light')
+// theme
+const isSystemDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+const currentTheme = ref(isSystemDark ? 'dark' : 'light')
+const systemTheme = ref(isSystemDark ? 'dark' : 'light')
 const themeProvider = document.createElement('style')
 themeProvider.id = 'c-theme-provider'
 document.head.appendChild(themeProvider)
 
+const global = document.getElementsByTagName('html')[0]
+
 export const useTheme = () => {
   return {
     currentTheme,
+    systemTheme,
     changeTheme(theme: 'light' | 'dark' | string) {
+      let darkIndex = global.className.indexOf('dark')
       if (theme == 'light') {
-        themeProvider.innerHTML = colors
         currentTheme.value = theme
+        if (darkIndex > -1)
+          global.className =
+            global.className.substring(0, darkIndex) +
+            global.className.substring(darkIndex + 4)
       } else if (theme == 'dark') {
-        themeProvider.innerHTML = colorsDark
         currentTheme.value = theme
+        if (darkIndex < 0) global.className += ' dark'
       } else {
-        themeProvider.innerHTML = theme
         currentTheme.value = 'custom'
+        themeProvider.innerHTML = theme
       }
     },
   }
 }
 
-// init theme
+// init theme with system
 const theme = useTheme()
-theme.changeTheme(currentTheme.value)
+theme.changeTheme(systemTheme.value)
 
-// watch theme change
+// watch system theme change
 var themeChanger = window.matchMedia('(prefers-color-scheme: dark)')
 themeChanger.addEventListener('change', (event) => {
   if (currentTheme.value == 'custom') return
   if (event.matches) {
     theme.changeTheme('dark')
+    systemTheme.value = 'dark'
   } else {
     theme.changeTheme('light')
+    systemTheme.value = 'light'
   }
 })
 
-// docs theme change
-var html = document.getElementsByTagName('html')[0]
-console.log(html)
-html.addEventListener('DOMAttributeNameChanged', (event) => {
-  console.log(event, html.className)
+// watch other theme change(手动赋予html class="dark" 等操作)
+const isDark = () => {
+  return global.className.indexOf('dark') > -1
+}
+const observer = new MutationObserver((mutationsList, observer) => {
+  for (let mutation of mutationsList) {
+    // console.log('attr change', mutation)
+    if (isDark() && currentTheme.value == 'light') theme.changeTheme('dark')
+    else if (!isDark() && currentTheme.value == 'dark')
+      theme.changeTheme('light')
+  }
 })
+const config = {
+  attributes: true,
+  childList: false,
+  subTree: false,
+}
+observer.observe(global, config)
