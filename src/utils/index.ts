201a9094f4b8558c32563e9791a53527ddbe5a65
congb19 <347006087@qq.com>
2023-03-31 10:52:42 +0800

    docs: build

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,4 @@
 export const c_log = (type: 'log' | 'warn' | 'error', msg: string) => {
   const res = '[Combo UI] ' + msg
-  switch (type) {
-    case 'log':
-      console.log(res)
-      break
-    case 'warn':
-      console.warn(res)
-      break
-    case 'error':
-      console.error(res)
-      break
-  }
+  console[type](res)
 }
